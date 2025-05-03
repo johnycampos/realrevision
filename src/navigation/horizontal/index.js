30fe94f437d30bb55c46678362eb97f69fc3b426
johnycampos <johny.campos@ymail.com>
2025-05-03 18:30:12 -0300

    Adicionando_estoque

--- src/navigation/horizontal/index.js
+++ src/navigation/horizontal/index.js
@@ -1,7 +1,9 @@
+
 import appAndPages from './app-and-pages'
 import charts from './charts'
 import dashboard from './dashboard'
+import estoque from './estoque'
 import forms from './forms'
 import others from './others'
 import uiElements from './ui-elements'
-export default [...dashboard, ...appAndPages, ...uiElements, ...forms, ...charts, ...others]
+export default [...dashboard, ...estoque, ...appAndPages, ...uiElements, ...forms, ...charts, ...others]
